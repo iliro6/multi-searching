122b4a90b3a3ac42550e73f72402ce464d9893a3
iliaroozban <94915568+iliaroozbann@users.noreply.github.com>
2024-03-13 01:03:17 +0330

    dropdown setup

---
+++ src/components/DropDown.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+const DropDown = () => {
+  return (
+    <Wrapper>DropDown</Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+    
+`
+
+export default DropDown
